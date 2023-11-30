db17526f89f478fffa8846f3e56bb40f70fd6e8c
ikramna17 <ikramnassih1017@gmail.com>
2023-11-30 18:35:45 +0000

    all

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+nclude "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a new node for a binary tree
+ * @parent: Pointer to the parent node
+ * @value: Value to be stored in the new node
+ *
+ * Return: Pointer to the newly created node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *nw;
+
+	nw = malloc(sizeof(binary_tree_t));
+	if (nw == NULL)
+		return (NULL);
+
+	nw->n = value;
+	nw->parent = parent;
+	nw->left = NULL;
+	nw->right = NULL;
+
+	return (nw);
+}
